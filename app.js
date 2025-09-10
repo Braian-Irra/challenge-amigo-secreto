8c12e90489ac6960747442a2238c330da8657cc5
Braian Irrazabal <braiianirrazabal@gmail.com>
2025-09-10 05:16:56 -0300

    Create agregarAmigo() function

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo(){
+    let nuevoAmigo = document.querySelector("#amigo").value;
+
+    if (nuevoAmigo === "") {
+        alert("Por favor ingrese un nombre")
+    }else {
+        alert("Amigo Agregado");
+    }
+}
