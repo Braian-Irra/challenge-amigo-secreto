4c25ca78ec7c655efdef1aebce3456627e7bb7b9
Braian Irrazabal <braiianirrazabal@gmail.com>
2025-09-10 21:10:59 -0300

    creo funcion agregarAmigos

--- app.js
+++ app.js
@@ -13,20 +13,20 @@ function agregarAmigo(){
         alert("Por favor ingrese un nombre")
     }else {
         amigos.push(nuevoAmigo);
-        limpiarCaja();
         console.log(amigos);
+        limpiarCaja();
         return nuevoAmigo;
     }
 }
 
 //funcion para actualizar la lista de amigos
 //recorre el array amigos y agrega cada nombre comom un elemento <li> dentro de la lista HTML
-function actualizarAmigos(){
+function agregarAmigos(){
     let lista = document.querySelector('#listaAmigos');
     lista.innerHTML = "";
 
     for (let i = 0; i < amigos.length; i++){
-        let li = document.createElement("li");;
+        let li = document.createElement("li");
         li.textContent = amigos[i];
         lista.appendChild(li);
     }
